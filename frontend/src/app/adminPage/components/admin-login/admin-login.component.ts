3d0083a3799bc85bb56ca3358d5340e4d0128b55
Thuch_o_o <91382929+Thadthon08@users.noreply.github.com>
2024-09-07 00:12:26 +0700

    Implement admin login feature with form validation and localStorage support

--- frontend/src/app/adminPage/components/admin-login/admin-login.component.ts
+++ frontend/src/app/adminPage/components/admin-login/admin-login.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-login',
   templateUrl: './admin-login.component.html',
-  styleUrl: './admin-login.component.css'
+  styleUrl: './admin-login.component.css',
 })
 export class AdminLoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: [
+        '',
+        [Validators.required, Validators.pattern('^[A-Z][a-zA-Z0-9]+$')],
+      ],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const user = {
+        username: this.loginForm.value.username,
+        role: 'admin',
+      };
+      localStorage.setItem('user', JSON.stringify(user));
+
+      console.log('Form Submitted!', this.loginForm.value);
+    } else {
+      console.log('Form is not valid');
+    }
+  }
 }
