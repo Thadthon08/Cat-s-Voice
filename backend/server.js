4fd0dd0d6c1b02645a472e4bc140c46dd35f55ab
B6409388 <theerabeem@gmail.com>
2024-09-07 13:23:48 +0700

    edit server

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const cors = require("cors");
+const authenticateJWT = require("./middleware/authenticateJWT");
 require("dotenv").config();
 
 const app = express();
@@ -9,9 +11,21 @@ connectDB();
 // ใช้ middleware สำหรับแปลงข้อมูล JSON
 app.use(express.json());
 
+// Middleware for JSON parsing
+app.use(cors()); // Enable CORS
+
 // เชื่อมต่อเส้นทาง API ของ Animal
 const animalRoutes = require("./routes/animalRoutes");
 app.use("/api/animals", animalRoutes);
+app.use("/api/admin", authenticateJWT, require("./routes/admin"));
 
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("Something went wrong!");
+  });
+
+  
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
