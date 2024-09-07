f8e0cd6a4463704abbf9208102a34be70b6f7dc5
Thuch_o_o <91382929+Thadthon08@users.noreply.github.com>
2024-09-07 19:49:00 +0700

    feat:FindHonmePage

---
+++ frontend/src/app/adminPage/components/add-data/add-data.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-data',
+  templateUrl: './add-data.component.html',
+  styleUrl: './add-data.component.css'
+})
+export class AddDataComponent {
+
+}
