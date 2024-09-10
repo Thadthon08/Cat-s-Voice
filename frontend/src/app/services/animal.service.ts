e06960014155f692033013cdaae62a2a97084947
Thuch_o_o <91382929+Thadthon08@users.noreply.github.com>
2024-09-11 00:27:44 +0700

    edit import Animalservice

--- frontend/src/app/services/animal.service.ts
+++ frontend/src/app/services/animal.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Animal } from '../interface/Ianimal';
+import { Animal } from '../interface/IAnimal';
 
 @Injectable({
   providedIn: 'root',
