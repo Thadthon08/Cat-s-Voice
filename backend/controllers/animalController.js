4a4ed490a5ce205f260a5b72fda3a2aadcef0012
Thuch_o_o <91382929+Thadthon08@users.noreply.github.com>
2024-09-14 16:57:25 +0700

    fix type HealRec

--- backend/controllers/animalController.js
+++ backend/controllers/animalController.js
@@ -1,14 +1,38 @@
 const Animal = require("../models/schema.js").Animal;
 const upload = require("../middlewares/uploadImage");
+const HealthRecord = require("../models/schema.js").HealthRecord;
 
 // GET - ดึงข้อมูลสัตว์ทั้งหมด
+
 exports.getAllAnimals = async (req, res) => {
   try {
     const status = req.query.status;
     const query = status ? { status: status } : {};
-
+    // ดึงข้อมูลสัตว์ตามเงื่อนไขที่กำหนด (เช่น status)
     const animals = await Animal.find(query);
-    res.status(200).json(animals);
+    // ใช้ Promise.all เพื่อดึงข้อมูล HealthRecord ของสัตว์แต่ละตัวพร้อมกัน
+    const animalInfoWithDiagnosis = await Promise.all(
+      animals.map(async (animal) => {
+        // ดึงข้อมูลการวินิจฉัยล่าสุดที่เกี่ยวข้องกับสัตว์แต่ละตัว
+        const healthRecord = await HealthRecord.findOne({
+          animal_id: animal._id,
+        }).sort({ checkup_date: -1 });
+
+        // สร้างวัตถุข้อมูลสัตว์รวมกับการวินิจฉัยล่าสุด
+        return {
+          name: animal.name,
+          gender: animal.gender,
+          age: animal.age,
+          color: animal.color,
+          personality: animal.personality,
+          status: animal.status,
+          image_url: animal.image_url,
+          diagnosis: healthRecord ? healthRecord.diagnosis : "none", // ถ้าไม่มีข้อมูลให้แสดง "none"
+        };
+      })
+    );
+
+    res.status(200).json(animalInfoWithDiagnosis);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
