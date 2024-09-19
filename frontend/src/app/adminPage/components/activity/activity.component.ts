455483075141ecad05ea99da168bd1bbcbbbf1f4
B6409388 <theerabeem@gmail.com>
2024-09-19 12:15:13 +0700

    งง

--- frontend/src/app/adminPage/components/activity/activity.component.ts
+++ frontend/src/app/adminPage/components/activity/activity.component.ts
@@ -5,58 +5,77 @@ import { ActivityService } from '../../../services/activity.service';
 @Component({
   selector: 'app-activity',
   templateUrl: './activity.component.html',
-  styleUrl: './activity.component.css'
+  styleUrls: ['./activity.component.css'],
 })
 export class ActivityComponent implements OnInit  {
 
   activitys: any[] = [];
-  status: string | null = null;
-  rows = 8; 
-  totalRecords = 0; 
-  currentPage = 1; 
+  // status: string | null = null;
+  // rows = 8; 
+  // totalRecords = 0; 
+  // currentPage = 1; 
   
 
   constructor(
     private router: Router,
     private activityService: ActivityService,
-    private route: ActivatedRoute
+    // private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    this.route.queryParamMap.subscribe((params) => {
-      this.status = params.get('status');
-      this.loadActivity();
-    });
+    // this.route.queryParamMap.subscribe((params) => {
+    //   this.status = params.get('status');
+    //   // this.loadActivity();
+      
+    // });
+    this.getActivitys();
   }
 
-  loadActivity(page: number = 1, rows: number = this.rows) {
-    this.activityService
-      .getActivity(this.status ?? undefined, rows, page)
-      .subscribe(
-        (data) => {
-          this.activitys = data.activitys;
-          this.totalRecords = data.totalRecords;
-          this.currentPage = data.currentPage;
-          this.rows = rows; 
-        },
-        (error) => {
-          console.error('Error fetching animals:', error);
-        }
-      );
+  // loadActivity(page: number = 1, rows: number = this.rows) {
+  //   this.activityService
+  //     .getActivity(this.status ?? undefined, rows, page)
+  //     .subscribe(
+  //       (data) => {
+  //         this.activitys = data.activitys;
+  //         this.totalRecords = data.totalRecords;
+  //         this.currentPage = data.currentPage;
+  //         this.rows = rows; 
+  //       },
+  //       (error) => {
+  //         console.error('Error fetching animals:', error);
+  //       }
+  //     );
+  // }
+
+  getActivitys(): void {
+    this.activityService.getActivitys().subscribe(
+      (data) => {
+        this.activitys = data.map((activity: any) => {
+          return {
+            _id: activity._id,
+            name: activity.animal_id.name,
+            image_url: activity.animal_id.image_url,
+          };
+        });
+      },
+      (error) => {
+        console.error('Error fetching health activitys:', error);
+      }
+    );
   }
 
-  paginate(event: any) {
-    this.currentPage = event.page + 1; 
-    this.loadActivity(this.currentPage, event.rows); 
-  }
+  // paginate(event: any) {
+  //   this.currentPage = event.page + 1; 
+  //   this.getActivitys(this.currentPage, event.rows); 
+  // }
 
   navigateToAddData() {
     this.router.navigate(['/admin/even/add-data']);
   }
 
-  onAnimalCardClick(id: number) {
+  onActivitysCardClick(id: number) {
     console.log('activity ID:', id);
-    this.router.navigate(['/admin/activity', id]);
+    this.router.navigate(['/admin/even', id]);
   }
 
 }
--- frontend/src/app/services/activity.service.ts
+++ frontend/src/app/services/activity.service.ts
@@ -25,21 +25,21 @@ export class ActivityService {
   }
 
 
-  getActivity(
-    status?: string,
-    limit: number = 10,
-    page: number = 1
-  ): Observable<any> {
-    let params = new HttpParams()
-      .set('limit', limit.toString())
-      .set('page', page.toString());
+  // getActivity(
+  //   status?: string,
+  //   limit: number = 10,
+  //   page: number = 1
+  // ): Observable<any> {
+  //   let params = new HttpParams()
+  //     .set('limit', limit.toString())
+  //     .set('page', page.toString());
 
-    if (status) {
-      params = params.set('status', status);
-    }
+  //   if (status) {
+  //     params = params.set('status', status);
+  //   }
 
-    return this.http.get<any>(this.baseUrl, { params });
-  }
+  //   return this.http.get<any>(this.baseUrl, { params });
+  // }
 
   getActivitys(): Observable<any> {
     return this.http.get<any>(this.baseUrl);
